5f9eaed487227932503d5d0e58175b6cb7a8de87
bots304yt <61882375+bots304yt@users.noreply.github.com>
2022-07-16 20:21:37 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const abme = document.querySelector('.btn');
+
+abme.addEventListener('click', function(){
+    console.log('hola')
+    document.getElementById('boton').classList.toggle('active')
+});
