dfe353035b0181ccc26bccb6fee66921525aaf10
bots304yt <61882375+bots304yt@users.noreply.github.com>
2022-07-22 20:59:43 +0200

    Add files via upload

--- tiempo/scripts.js
+++ tiempo/scripts.js
@@ -1,1 +1,161 @@
-var requestOptions2={method:"GET",headers:{"x-api-key":"5a56d0177f27d7692ed9c58f542696b542d6b8ede69cc43b1d0540defe53544a","Content-type":"application/json"}};function tiempo(){navigator.geolocation.getCurrentPosition(function(e){var t=e.coords.latitude,e=e.coords.longitude;fetch("https://api.ambeedata.com/weather/latest/by-lat-lng?lat="+t+"&lng="+e,requestOptions2).then(e=>e.json()).then(e=>(document.getElementById("resultado").innerHTML=(5*(e.data.temperature-32)/9).toFixed(0)+"")&&(document.getElementById("snr").innerHTML=(5*(e.data.apparentTemperature-32)/9).toFixed(0)+"°C")&&(document.getElementById("humr").innerHTML=(100*e.data.humidity).toFixed(0)+"%")&&(document.getElementById("visibr").innerHTML=e.data.visibility.toFixed(0)+"km")&&(document.getElementById("uvr").innerHTML=e.data.uvIndex.toFixed(0)+"")&&(document.getElementById("vientor").innerHTML=e.data.windSpeed.toFixed(0)+"mpH")&&setCorrectMargin()&&console.log(t+" "+e)).catch(e=>console.log("error",e)),getDay()})}function getDay(){var e,t=new Date;switch(t.getMonth()+1){case 1:e="enero";break;case 2:e="febrero";break;case 3:e="marzo";break;case 4:e="abril";break;case 5:e="mayo";break;case 6:e="junio";break;case 7:e="julio";break;case 8:e="agosto";break;case 9:e="septiembre";break;case 10:e="octubre";break;case 11:e="noviembre";break;case 12:e="diciembre"}t=t.getDate()+" de "+e,document.getElementById("day").innerHTML=t}function setCorrectMargin(){console.log("hola"),document.getElementById("snr").innerHTML<10?document.getElementById("snr").style.marginLeft="2.6vw":document.getElementById("snr").style.marginLeft="1.8vw",document.getElementById("humr").innerHTML<10?document.getElementById("humr").style.marginLeft="2.6vw":document.getElementById("humr").style.marginLeft="1.8vw",document.getElementById("vientor").style.marginLeft="1vw",document.getElementById("visibr").style.marginLeft="1vw",console.log(document.getElementById("humr").textContent),console.log(document.getElementById("snr").textContent),console.log(document.getElementById("vientor").textContent),console.log(document.getElementById("visibr").textContent)}
+var requestOptions2 = { method: "GET", headers: { "x-api-key": "5a56d0177f27d7692ed9c58f542696b542d6b8ede69cc43b1d0540defe53544a", "Content-type": "application/json" } };
+function tiempo() {
+    setCorrectMargin();
+    navigator.geolocation.getCurrentPosition(function (e) {
+        var t = e.coords.latitude,
+            e = e.coords.longitude;
+        fetch("https://api.ambeedata.com/weather/latest/by-lat-lng?lat=" + t + "&lng=" + e, requestOptions2)
+            .then((e) => e.json())
+            .then(
+                (e) =>
+                    (document.getElementById("resultado").innerHTML = ((5 * (e.data.temperature - 32)) / 9).toFixed(0) + "") &&
+                    (document.getElementById("snr").innerHTML = ((5 * (e.data.apparentTemperature - 32)) / 9).toFixed(0) + "°C") &&
+                    (document.getElementById("humr").innerHTML = (100 * e.data.humidity).toFixed(0) + "%") &&
+                    (document.getElementById("visibr").innerHTML = e.data.visibility.toFixed(0) + "km") &&
+                    (document.getElementById("uvr").innerHTML = e.data.uvIndex.toFixed(0) + "") &&
+                    (document.getElementById("vientor").innerHTML = e.data.windSpeed.toFixed(0) + "mpH") &&
+                    (setCorrectMargin()))
+            .catch((e) => console.log("error", e)),
+            getDay();
+    });
+}
+function getDay() {
+    var e,
+        t = new Date();
+    switch (t.getMonth() + 1) {
+        case 1:
+            e = "ENERO";
+            break;
+        case 2:
+            e = "FEBRERO";
+            break;
+        case 3:
+            e = "MARZO";
+            break;
+        case 4:
+            e = "ABRIL";
+            break;
+        case 5:
+            e = "MAYO";
+            break;
+        case 6:
+            e = "JUNIO";
+            break;
+        case 7:
+            e = "JULIO";
+            break;
+        case 8:
+            e = "AGOSTO";
+            break;
+        case 9:
+            e = "SEPTIEMBRE";
+            break;
+        case 10:
+            e = "OCTUBRE";
+            break;
+        case 11:
+            e = "NOVIEMBRE";
+            break;
+        case 12:
+            e = "DICIEMBRE";
+    }
+    var wd;
+    switch (t.getDay()) {
+        case 1:
+            wd = "LUN.";
+            break;
+        case 2:
+            wd = "MAR.";
+            break;
+        case 3:
+            wd = "MIE.";
+            break;
+        case 4:
+            wd = "JUE.";
+            break;
+        case 5:
+            wd = "VIE.";
+            break;
+        case 6:
+            wd = "SAB.";
+            break;
+        case 7:
+            wd = "DOM.";
+            break;
+    }
+    (t = (wd) +", "+ t.getDate() + " DE " + e), (document.getElementById("day").innerHTML = t);
+    getHour();
+}
+function getHour() {
+    t = new Date();
+    document.getElementById("hora").innerHTML = (t.getHours() + ":" + t.getMinutes());
+
+}
+
+function setCorrectMargin(){
+    var x = window.matchMedia("(min-device-width : 320px) and (max-device-width : 680px)")
+    var snr = document.getElementById("snr");
+    var humr = document.getElementById("humr");
+    var vientor = document.getElementById("vientor");
+    var visibr = document.getElementById("visibr");
+    var cels = document.getElementById("cels");
+    var resultado = document.getElementById("resultado");
+    var error = document.getElementById("error");
+
+        if (x.matches) { 
+            if(snr.innerHTML < 10){
+                snr.style.marginLeft = "11.6vw";
+            }else{
+                snr.style.marginLeft = "5.8vw";
+        
+            }
+            if(humr.innerHTML < 10){
+                humr.style.marginLeft = "11.6vw";
+            }else{
+                humr.style.marginLeft = "5.8vw";
+            }
+            if(vientor.innerHTML < 10){
+                vientor.style.marginLeft = "11.6vw";
+            }else{
+                vientor.style.marginLeft = "2.8vw";
+            }
+            if(visibr.innerHTML < 10){
+                visibr.style.marginLeft = "11.6vw";
+            }else{
+                visibr.style.marginLeft = "3.8vw";
+            }
+            if(resultado.innerHTML < 10){
+                cels.style.marginLeft = "13vw";
+            }else{
+                cels.style.marginLeft = "23vw";
+            }
+        } else {
+            if(snr.innerHTML < 10){
+                snr.style.marginLeft = "2.6vw";
+            }else{
+                snr.style.marginLeft = "1.8vw";
+        
+            }
+            if(humr.innerHTML < 10){
+                humr.style.marginLeft = "2.6vw";
+            }else{
+                humr.style.marginLeft = "1.8vw";
+            }
+            if(vientor.innerHTML < 10){
+                vientor.style.marginLeft = "2.6vw";
+            }else{
+                vientor.style.marginLeft = "1vw";
+        
+            }
+            if(visibr.innerHTML < 10){
+                visibr.style.marginLeft = "2.6vw";
+            }else{
+                visibr.style.marginLeft = "1vw";
+            }
+        }
+        if(snr.innerHTML == 0 && vientor.innerHTML == 0 && humr.innerHTML == 0 && visibr.innerHTML == 0 && resultado.innerHTML == 0){
+            error.style.display = "block";
+        }else{
+            error.style.display = "none";
+        }
+}
