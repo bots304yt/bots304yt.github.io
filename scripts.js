673ec46c7e5da87f895f1ff20cfc85a83fecf1c5
bots304yt <61882375+bots304yt@users.noreply.github.com>
2022-08-20 15:39:28 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-function tp() {
-    console.log(document.getElementById("text").value + "");
-    window.location.href = 'bots304yt.github.io/hola.php?mensaje=' + document.getElementById("text").value;
-}
